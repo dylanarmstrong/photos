8c03ab72503fc36270dc7f3cef999f99e6f4a87e
Dylan Armstrong <dylan@dylan.is>
2024-02-28 23:31:47 +0100

    fix: actualy fix etag stuff

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,34 +1,16 @@
 import compression from 'compression';
-import etag from 'etag';
 import express from 'express';
 import helmet from 'helmet';
 import { join } from 'node:path';
-import { randomBytes } from 'node:crypto';
 
 import { router } from './routes.js';
 import { baseUrl, isDevelopment, port } from './constants.js';
 
 const app = express();
 
-// Used for nonce to ensure it doesn't work if undefined
-const rnd = randomBytes(16).toString('hex');
-
 app.set('view engine', 'pug');
 app.set('views', join(process.cwd(), 'views'));
-// Remove nonces from the buffer before generating etags
-app.set('etag', (body: string | Buffer, encoding?: BufferEncoding) => {
-  const buffer = Buffer.from(
-    String(body).replaceAll(/nonce=".*?"/g, ''),
-    encoding,
-  );
-  return etag(buffer, { weak: false });
-});
 app.use(compression());
-
-app.use((_, response, next) => {
-  response.locals['nonce'] = randomBytes(16).toString('hex');
-  next();
-});
 
 app.use(
   helmet({
@@ -37,10 +19,7 @@ app.use(
         // eslint-disable-next-line unicorn/no-null
         'connect-src': isDevelopment ? ['ws://localhost:5173/'] : null,
         'img-src': ["'self'", 'photos.dylan.is'],
-        'script-src': [
-          "'self'",
-          (_, response) => `'nonce-${response.locals.nonce || rnd}'`,
-        ],
+        'script-src': isDevelopment ? ["'self'", 'localhost:5173/'] : ["'self'"],
         // eslint-disable-next-line unicorn/no-null
         'upgrade-insecure-requests': isDevelopment ? null : [],
       },
