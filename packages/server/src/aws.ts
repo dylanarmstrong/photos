de3594d1daa55a0ee71026b3914a3cee01abcab7
Dylan Armstrong <dylan@dylan.is>
2024-03-12 07:58:11 +0100

    fix: aws replace was wrong

--- packages/server/src/aws.ts
+++ packages/server/src/aws.ts
@@ -25,9 +25,9 @@ const s3 = new S3Client({
 
 const photoMap = ({ Key, Size }: _Object): string | undefined => {
   // Gather all valid images by finding 320 sized thumbnails
-  if (Size && Size > 0 && Key && Key.endsWith('_w320.avif')) {
+  if (Size && Size > 0 && Key && Key.endsWith('_w320.jpeg')) {
     // Return without the thumbnail portion of the Key
-    return Key.replace('_512.jpeg', '.jpeg');
+    return Key.replace('_w320.jpeg', '.jpeg');
   }
 
   return undefined;
